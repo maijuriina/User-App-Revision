21b851c7f5021f2fb9388c1d628520a4266d77f5
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2022-04-08 13:54:59 +0300

    Change to css modules, added label and id for accessibility

--- user-app-revision/src/components/NewUser/UserForm.js
+++ user-app-revision/src/components/NewUser/UserForm.js
@@ -21,6 +21,7 @@ const UserForm = (props) => {
 
   const submitHandler = (event) => {
     event.preventDefault();
+
     const userFormData = {
       userName: userInput.userName,
       userAge: userInput.userAge,
@@ -37,15 +38,17 @@ const UserForm = (props) => {
 
   return (
     <form onSubmit={submitHandler}>
-      <label>Username</label>
+      <label htmlFor="username">Username</label>
       <input
+        id="username"
         type="text"
         value={userInput.userName}
         onChange={userNameChangeHandler}
       ></input>
 
-      <label>Age</label>
+      <label htmlFor="age">Age</label>
       <input
+        id="age"
         type="number"
         min="0"
         value={userInput.userAge}
