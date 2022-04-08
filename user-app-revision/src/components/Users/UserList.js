cd327568b917cc61620905038e8cff52b4ffeb38
Maijuriina Mustonen <42972494+maijuriina@users.noreply.github.com>
2022-04-08 14:05:13 +0300

    Styling added for button and empty data

--- user-app-revision/src/components/Users/UserList.js
+++ user-app-revision/src/components/Users/UserList.js
@@ -1,10 +1,15 @@
 import "./UserList.css";
+import Card from "../UI/Card";
 import UserItem from "./UserItem";
 import React from "react";
 
 const UserList = (props) => {
   if (props.items.length === 0) {
-    return <h3>No user data to show.</h3>;
+    return (
+      <Card>
+        <h3>No user data to show.</h3>
+      </Card>
+    );
   }
 
   return (
